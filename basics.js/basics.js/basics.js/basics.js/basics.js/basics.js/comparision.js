0863db1e4753bbb7609f2db1b1cf312a29cbf702
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-03 08:16:40 +0000

    basics

---
+++ basics.js/basics.js/basics.js/basics.js/basics.js/basics.js/comparision.js
@@ -0,0 +1,8 @@
+//console.log(2>=1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+//console.log(undefined==null);
+//console.log(undefined===null);
+//console.log(undefined!==null);
+//console.log(undefined==undefined);
---
+++ basics.js/variables.js
@@ -0,0 +1,10 @@
+const accountID=144553
+let accountEmail="john@example.com"
+var accountPassword="123456"
+accountCity="jaipur"
+
+accountEmail="jane@example.com"
+accountPassword="2121212"
+accountCity="pune"
+console.log(accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity])
