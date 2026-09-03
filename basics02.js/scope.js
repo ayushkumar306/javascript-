dd63b6deb0fc9d19cff81a10535886ebc259dc13
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-03 10:51:49 +0000

    basics02

---
+++ basics02.js/scope.js
@@ -0,0 +1,41 @@
+let a=50
+if(true){
+    let a=10
+    const b =20
+    var c=30
+    //console.log("INNER:",a)
+}
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+function one(){
+    const username="monu"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+//one()
+if(true){
+    const username="monu"
+    if(username==="monu"){
+        const website="youtube"
+        //console.log(username+website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+
+
+function addOne(num){
+    return num+1
+}
+addOne(5)
+
+const addTwo=function(num){
+    return num+2
+}
+addTwo(5)
