1554845ff11c64ccf363b1f57e78507375a182d5
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-03 11:43:27 +0000

    basics02

---
+++ basics02.js/iife.js
@@ -0,0 +1,9 @@
+//immeditely invoked function expressions(IIFE)
+//named iife
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();
+//unnamed iife
+(()=>{
+    console.log(`DB CONNECTED TWO`)
+})()
