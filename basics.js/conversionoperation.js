10827b0f5467c6c8c616d0b1c26348591b2e9d65
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-03 08:39:16 +0000

    basics

---
+++ basics.js/conversionoperation.js
@@ -0,0 +1,35 @@
+let score="monu"
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber=123
+let stringNumber=String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//operations
+let value=45
+let negvalue=-value
+//console.log(negvalue);
+//console.log(2+3);
+//console.log("2"+3);
+//console.log(1+2+"3");
+//console.log(+true);
+
+let str1="hello"
+let str2="world"
+let str3=str1+str2
+//console.log(str3);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
