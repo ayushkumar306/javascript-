291ec66bdd8f5fcab2cd21d82607f71948fa9f1a
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-03 09:26:48 +0000

    basics01

---
+++ basics01.js/objects1.js
@@ -0,0 +1,15 @@
+const course={
+    coursenme:"js in hindi",
+    price:"999",
+    courseInstructor:"monu"
+}
+//course.courseInstructor
+const{courseInstructor}=course
+console.log(courseInstructor)
+
+//json api 
+//{
+//    "name":"monu",
+//    "age":"22",
+//    "gender":"male"
+//}
