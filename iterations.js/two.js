3089cf786f3c85bad3034a3a37f7aa02dd56bbaf
ayushkumar306 <ayushkumar56973@gmail.com>
2026-09-04 11:17:17 +0000

    iterations

---
+++ iterations.js/two.js
@@ -0,0 +1,20 @@
+//do while loop
+//while
+let index=0
+while(index<=10){
+    //console.log(`value of index is ${index}`)
+    index=index+2
+}
+
+let myarray=["flash","batman","superman"]
+let arr=0
+while(arr<myarray.length){
+    //console.log(`value of arr is ${myarray[arr]}`)
+    arr=arr+1
+}
+
+let score=1
+do{
+console.log(`value of score is ${score}`)
+score=score+1
+}while(score<=10)
